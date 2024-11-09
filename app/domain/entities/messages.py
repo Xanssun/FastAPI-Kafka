6db0fc5108772fc08780320d0e0fe5f08226e6df
xanssun <xanssun@yandex.ru>
2024-11-09 19:52:44 +0500

    fix 3

--- app/domain/entities/messages.py
+++ app/domain/entities/messages.py
@@ -7,12 +7,17 @@ from domain.values.messages import Text, Title
 @dataclass
 class Message(BaseEntity):
     text: Text
+    # _hash_ = BaseEntity.__hash__
 
 
 @dataclass
 class Chat(BaseEntity):
     title: Title
+    # _hash_ = BaseEntity.__hash__
     message: set[Message] = field(
-        default_factory=set, # не уверен что лучше оставить set
+        default_factory=set,
         kw_only=True,
     )
+
+    def add_message(self, message: Message):
+        self.message.add(message)
