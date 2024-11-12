bb6ef376a36f80eaf86024e4bf20e471cf421d9e
xanssun <xanssun@yandex.ru>
2024-11-12 19:25:35 +0500

    base logic + mediator

---
+++ app/logic/mediator.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from collections.abc import Iterable
+from dataclasses import dataclass, field
+
+from logic.commands.base import CR, CT, CommandHandler
+from logic.events.base import ER, ET, EventHandler
+
+
+@dataclass(eq=False)
+class Mediator:
+    events_map: dict[ET, EventHandler] = field(
+        default_factory=lambda: defaultdict(list),
+        kw_only=True,
+    )
+    commands_map: dict[CT, CommandHandler] = field(
+        default_factory=lambda: defaultdict(list),
+        kw_only=True,
+    )
+
+    def register_event(self, event: ET, event_handlers: Iterable[EventHandler[ET, ER]]):
+        self.events_map[event].append(event_handlers)
+
+    def register_command(self, command: CT, command_handlers: Iterable[CommandHandler[CT, CR]]):
+        self.events_map[command].extend(command_handlers)
