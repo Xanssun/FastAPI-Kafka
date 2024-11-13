54116eb65282b80ed46fee6d92b9d9a9c82113f6
xanssun <xanssun@yandex.ru>
2024-11-14 03:13:46 +0500

    + test fix

--- app/tests/application/api/test_messages.py
+++ app/tests/application/api/test_messages.py
@@ -1,6 +1,6 @@
 import pytest
 from faker import Faker
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.testclient import TestClient
 from httpx import Response
 
@@ -19,3 +19,34 @@ def test_create_chat_success(
     json_data = response.json()
 
     assert json_data['title'] == title
+
+
+@pytest.mark.asyncio
+def test_create_chat_fail_text_too_long(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    title = faker.text(max_nb_chars=500)
+    response: Response = client.post(url=url, json={'title': title})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    json_data = response.json()
+
+    assert json_data['detail']['error']
+
+
+@pytest.mark.asyncio
+def test_create_chat_fail_text_empty(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    response: Response = client.post(url=url, json={'title': ''})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    json_data = response.json()
+
+    assert json_data['detail']['error']
